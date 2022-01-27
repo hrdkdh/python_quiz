93e4065439c7723690a0eec030ada7104348bf2f
hrdkdh <36975040+hrdkdh@users.noreply.github.com>
2022-01-27 20:57:51 +0900

    update

---
+++ python_quiz/python_quiz/python_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  python_quizApp.swift
+//  python_quiz
+//
+//  Created by hrdkdh on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct python_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
